e59f0abcec172f49699d905196b35f317f2d5914
atendimentokta-alt <atendimentokta@gmail.com>
2026-09-06 20:33:40 -0300

    Delete uniformes-esportivos/modelos.js

--- uniformes-esportivos/modelos.js
+++
@@ -1,6 +0,0 @@
-window.MEGA_MODELOS=[
-{id:"atletico",nome:"Atlético Concept",arquivo:"assets/modelos/atletico-madrid.jpg",frente:{x:0,y:285,w:975,h:1329},costas:{x:1070,y:285,w:978,h:1329}},
-{id:"barcelona",nome:"Barcelona Concept",arquivo:"assets/modelos/barcelona.png",frente:{x:10,y:114,w:268,h:367},costas:{x:302,y:114,w:270,h:367}},
-{id:"milan",nome:"Milan Concept Red",arquivo:"assets/modelos/milan-concept-red.png",frente:{x:12,y:93,w:234,h:318},costas:{x:264,y:93,w:234,h:318}},
-{id:"italia",nome:"Italia 2020",arquivo:"assets/modelos/italia-2020.png",frente:{x:5,y:93,w:232,h:318},costas:{x:243,y:93,w:234,h:318}}
-];
